0d63bc9206a4e0bfdb1d9041b83b1e8fc9489ecb
Steffen Müller <smueller@cpan.org>
2007-01-21 17:38:05 +0000

    Import of SMUELLER/Math-GammaFunction-0.02 from CPAN.
    gitpan-cpan-distribution: Math-GammaFunction
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SMUELLER/Math-GammaFunction-0.02.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Math/GammaFunction.pm
+++ lib/Math/GammaFunction.pm
@@ -5,6 +5,8 @@ use warnings;
 
 require Exporter;
 
+our $VERSION = '0.02';
+
 our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw(
     gamma
@@ -15,7 +17,6 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 ) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Math::GammaFunction', $VERSION);
